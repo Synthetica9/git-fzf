9271528c7c9cba386adbd39da617f482c122b1ae
Patrick Hilhorst <git@hilhorst.be>
2018-12-04 13:56:42 +0100

    Initial code dump

---
+++ git-fzf.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+STRIP_REFLOG='grep "[0-9a-f]+" --perl-regexp --only-matching | head -n 1 | xargs git rev-parse'
+PREVIEWCOMMIT="echo \"{}\" | $STRIP_REFLOG | xargs git show --color=always"
+LOG_FORMAT="format:%C(auto,yellow)%h %C(auto,blue)%>(12)%ad %C(auto,green)%<(18) %aN%C(auto,reset)%s%C(auto,red)% gD% D"
+
+function fzfCommit { fzf --ansi --preview="$PREVIEWCOMMIT" --tiebreak=index; }
+
+if [ $# -lt 1 ];
+then
+  echo "No subcommand specified!" > /dev/stderr
+  exit 1
+fi
+
+
+subcommand=$1
+shift
+case $subcommand in
+  "reflog")
+    git reflog --color=always --format="$LOG_FORMAT" --date=relative $@ | fzfCommit | eval $STRIP_REFLOG
+    ;;
+  "log")
+    git log --color=always --format="$LOG_FORMAT" --date=relative $@ | fzfCommit | eval $STRIP_REFLOG
+    ;;
+  *)
+    echo "Unknown command '$subcommand'" > /dev/stderr
+    exit 1
+    ;;
+esac
